c522d7af81e66af311f67367163fad4329593f30
Michael Hatch <michaelhatch@me.com>
2021-07-02 10:20:46 -0700

    add location tile data

--- data.locationTiles.js
+++ data.locationTiles.js
@@ -1,1 +1,77 @@
-export default [];
+export default [
+  {
+    cost: {
+      blue: 4,
+      yellow: 4,
+    },
+    infinityPoints: 3,
+    name: "Avengers Tower, NYC",
+  },
+  {
+    cost: {
+      purple: 4,
+      red: 4,
+    },
+    infinityPoints: 3,
+    name: "Attilan",
+  },
+  {
+    cost: {
+      orange: 4,
+      yellow: 4,
+    },
+    infinityPoints: 3,
+    name: "Hell's Kitchen, NYC",
+  },
+  {
+    cost: {
+      orange: 4,
+      purple: 4,
+    },
+    infinityPoints: 3,
+    name: "Triskelion",
+  },
+  {
+    cost: {
+      orange: 4,
+      yellow: 4,
+    },
+    infinityPoints: 3,
+    name: "Hell's Kitchen, NYC",
+  },
+  {
+    cost: {
+      blue: 4,
+      red: 4,
+    },
+    infinityPoints: 3,
+    name: "Wakanda",
+  },
+  {
+    cost: {
+      orange: 3,
+      purple: 3,
+      yellow: 3,
+    },
+    infinityPoints: 3,
+    name: "Asgard",
+  },
+  {
+    cost: {
+      blue: 3,
+      red: 3,
+      yellow: 3,
+    },
+    infinityPoints: 3,
+    name: "Knowhere",
+  },
+  {
+    cost: {
+      blue: 3,
+      purple: 3,
+      red: 3,
+    },
+    infinityPoints: 3,
+    name: "Atlantis",
+  },
+];
