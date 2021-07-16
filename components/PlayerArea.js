3a1be135100554ea3ed41d82650afd4d5d8b0a94
Michael Hatch <michaelhatch@me.com>
2021-07-15 21:17:50 -0700

    update

--- components/PlayerArea.js
+++ components/PlayerArea.js
@@ -5,6 +5,57 @@ export default function PlayerArea(props) {
   return (
     <div>
       <h2>{props.player.getName()}</h2>
+      <h4>Tokens</h4>
+      <p>
+        Gray:{" "}
+        {
+          props.gameState.ownerTracker.tokens.gray.filter(
+            (owner) => owner === props.player
+          ).length
+        }
+      </p>
+      <p>
+        Yellow:{" "}
+        {
+          props.gameState.ownerTracker.tokens.yellow.filter(
+            (owner) => owner === props.player
+          ).length
+        }
+      </p>
+      <p>
+        Red:{" "}
+        {
+          props.gameState.ownerTracker.tokens.red.filter(
+            (owner) => owner === props.player
+          ).length
+        }
+      </p>
+      <p>
+        Orange:{" "}
+        {
+          props.gameState.ownerTracker.tokens.orange.filter(
+            (owner) => owner === props.player
+          ).length
+        }
+      </p>
+      <p>
+        Blue:{" "}
+        {
+          props.gameState.ownerTracker.tokens.blue.filter(
+            (owner) => owner === props.player
+          ).length
+        }
+      </p>
+      <p>
+        Purple:{" "}
+        {
+          props.gameState.ownerTracker.tokens.purple.filter(
+            (owner) => owner === props.player
+          ).length
+        }
+      </p>
+      <h4>Recruits</h4>
+      <h4>Reserves</h4>
       <p>
         If it's this player's turn, this block should be highlighted and a list
         of options should be shown
