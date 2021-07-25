ae468edcb45cf29e4354342a3bfebc41de3840ae
Michael Hatch <michaelhatch@me.com>
2021-07-25 16:51:45 -0700

    de-annoy trainer switching

--- src/trainer/deprep.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-const packageJson = JSON.parse(fs.readFileSync("package.json"));
-if (packageJson.type) {
-  delete packageJson.type;
-}
-fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
--- src/trainer/prep.js
+++ src/trainer/prep.js
@@ -1,4 +1,11 @@
-const fs = require("fs");
-const packageJson = JSON.parse(fs.readFileSync("package.json"));
-packageJson.type = "module";
-fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
+import("fs").then((fs) => {
+  const packageJson = JSON.parse(fs.readFileSync("package.json"));
+  if (process.argv[2] === "--modulize") {
+    packageJson.type = "module";
+  } else {
+    if (packageJson.type) {
+      delete packageJson.type;
+    }
+  }
+  fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
+});
